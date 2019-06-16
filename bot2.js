e379401ba1d9a3a361200accdd74f7f6add2dccf
haytada <51841027+haytada@users.noreply.github.com>
2019-06-16 22:09:34 +0100

    Add files via upload

--- bot2.js
+++ bot2.js
@@ -6,10 +6,12 @@ client.on('ready', () => {
   console.log("I am online!")
 });
 
+//NEW MEMBER
 client.on('guildMemberAdd', member => {
     console.log('User' + member.user.tag + 'has joined the server!');
     var role = member.guild.roles.find('name', 'Members');
     member.addRole(role);
+    member.send("Welcome to the server, you can type !help to get a list of commands to use the Bot for, alternatively read the instructions in the Welcome channel for access to game specific channels!");
   });
 
 client.on('message', async message => { //ALL COMMANDS IN HERE
@@ -18,16 +20,21 @@ client.on('message', async message => { //ALL COMMANDS IN HERE
     const command = args.shift().toLowerCase(); 
 
     if(command === "say") {
+      if(!message.member.roles.some(r =>["Admin"].includes(r.name)))
+      return message.reply("Sorry, you don't have permission to use this command!")
         const sayMessage = args.join(" ");
         message.delete().catch(O_o=>{});
         message.channel.send(sayMessage);
     }
 
     if(command === "ping") {
-        message.channel.send("Pong!") 
-         }
+      const m = await message.channel.send("Ping?");
+      m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+    }
 
     if(command === "purge") {
+      if(!message.member.roles.some(r =>["Admin"].includes(r.name)))
+      return message.reply("Sorry, you don't have permission to use this command!")
         const deleteCount = parseInt(args[0], 10);
             if(!deleteCount || deleteCount < 2 || deleteCount > 100)
             return message.reply("Please provide a number between 2 and 100 for the number of messages to purge");
@@ -37,11 +44,11 @@ client.on('message', async message => { //ALL COMMANDS IN HERE
     }
 
     if(command === "help") {
-        message.reply("```Bot Commands:\n\n!say - Make the bot say what you type afterwards.\n!ping - Make the bot say Pong!\n!purge 2-100 - Delete specified number of messages.```")
+        message.reply("```Bot Commands:\n\n!ping - Check the latency of the bot and the API.\n!psnstatus - Check the status of the PlayStation Network.```")
     }
 
     if(command === "kick") {
-        if(!message.member.roles.some(r=>["Administrator", "BOT"].includes(r.name)))
+        if(!message.member.roles.some(r=>["Admin"].includes(r.name)))
         return message.reply("Sorry, you don't have permission to use this!");
         let member = message.mentions.members.first() || message.guild.members.get(args[0]);
         if(!member)
@@ -54,8 +61,26 @@ client.on('message', async message => { //ALL COMMANDS IN HERE
         .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
         message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
     }
+
+    if (command === "psnstatus") {
+      var getJSON = require('get-json')
+      const m = await message.channel.send("Pulling PlayStation Network status, please wait...");
+      getJSON("https://status.playstation.com/data/statuses/region/SCEE/GB/statusIndicator.json").then(function(response) {
+          var status;
+          switch(response.statusType) {
+              case "OK": status = "Online"; break;
+              default: status = response.statusType; break;
+          }
+          m.edit("PlayStation Network Status: **" + status + "**.");
+      }).catch(function(error) {
+          m.edit("Unable to pull PlayStation Network Status.").then(msg => msg.delete(10000));
+      });
+      return;
+  }
+
 });
 
+//DERRR
 client.on('message', (message) => {
   if(message.author.bot) return;
 
@@ -64,7 +89,8 @@ client.on('message', (message) => {
   }
  });
 
-client.on('raw', event => {
+//ROLE ADD/REMOVE REACTION
+ client.on('raw', event => {
     console.log(event);
     const eventName = event.t;
     if(eventName === 'MESSAGE_REACTION_ADD')
@@ -116,11 +142,11 @@ client.on('messageReactionAdd', (messageReaction, user) => {
       var member = messageReaction.message.guild.members.find(member => member.id === user.id);
       if(member)
       {
-        member.addRole(role.id);
+        member.addRole(role.id)
         console.log("Success. Added role.");
       }
     }
- });
+});
 
 client.on('messageReactionRemove', (messageReaction, user) => {
 
@@ -136,6 +162,28 @@ client.on('messageReactionRemove', (messageReaction, user) => {
     }
   }
 
+});
+
+//ACTIVITY
+const activities_list = [
+  "Rocket League",
+  "Cyberpunk 2077", 
+  "Rust",
+  "Rainbow Six Siege",
+  "Star Wars Jedi: Fallen Order",
+  "The Last of Us Part II",
+  "Death Stranding",
+  "DOOM Eternal",
+  "Borderlands 3",
+  "Gears 5",
+  "FIFA 20"
+  ]; 
+
+client.on('ready', () => {
+  setInterval(() => {
+      const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); 
+      client.user.setActivity(activities_list[index]);
+  }, 120000);
 });
 
 client.login(config.token)
